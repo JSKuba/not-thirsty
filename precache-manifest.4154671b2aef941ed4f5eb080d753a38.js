2404cb4240ff4a8a6fef87c21afa829aa0526b6a
JSKuba <jakub1chmielewski@gmail.com>
2020-06-03 14:17:25 +0200

    Updates

---
+++ precache-manifest.4154671b2aef941ed4f5eb080d753a38.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3401664e4772caef8fc7da3dd3684b5b",
+    "url": "/not-thirsty/index.html"
+  },
+  {
+    "revision": "03ba89ed30048908424d",
+    "url": "/not-thirsty/static/css/main.ad619481.chunk.css"
+  },
+  {
+    "revision": "4b0c0560f094c63f68dc",
+    "url": "/not-thirsty/static/js/2.5008bfa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/not-thirsty/static/js/2.5008bfa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ba89ed30048908424d",
+    "url": "/not-thirsty/static/js/main.353eaa20.chunk.js"
+  },
+  {
+    "revision": "13ffbb8de03c62ce33ef",
+    "url": "/not-thirsty/static/js/runtime-main.bf6b7c2c.js"
+  },
+  {
+    "revision": "dc0e6be90a4eb1829ca025304c8411b2",
+    "url": "/not-thirsty/static/media/about.dc0e6be9.jpg"
+  },
+  {
+    "revision": "c2ddc4371a3855a2e6970d942d57b6e3",
+    "url": "/not-thirsty/static/media/bg-1-s.c2ddc437.svg"
+  },
+  {
+    "revision": "c7f9cf868b90c1b0e1ed1a113035f77d",
+    "url": "/not-thirsty/static/media/bg-1.c7f9cf86.svg"
+  },
+  {
+    "revision": "bd03cd56703e636d3c10b3f515e901b0",
+    "url": "/not-thirsty/static/media/bg-2-s.bd03cd56.svg"
+  },
+  {
+    "revision": "ff8185e2ea8e64b290d0184fff682046",
+    "url": "/not-thirsty/static/media/bg-2.ff8185e2.svg"
+  },
+  {
+    "revision": "a4c7044029b71dbe6f9f80d74608681f",
+    "url": "/not-thirsty/static/media/bg-3-s.a4c70440.svg"
+  },
+  {
+    "revision": "89cb7802ed5cb3a58e03b1922b709b6c",
+    "url": "/not-thirsty/static/media/bg-3.89cb7802.svg"
+  },
+  {
+    "revision": "7ca9d4dff259f2f95d3a901f811efbcf",
+    "url": "/not-thirsty/static/media/bg-4.7ca9d4df.svg"
+  },
+  {
+    "revision": "e97f03060b2949b3921bab2e5cff5b31",
+    "url": "/not-thirsty/static/media/contact.e97f0306.jpg"
+  },
+  {
+    "revision": "11be3daf3ccbfc8cd4bd1c199671284b",
+    "url": "/not-thirsty/static/media/logo.11be3daf.svg"
+  },
+  {
+    "revision": "47f8a402d763a8eb872ccf89138e96a0",
+    "url": "/not-thirsty/static/media/shadow.47f8a402.svg"
+  }
+]);
